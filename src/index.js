f1a4810c1995295db3d8a6bb4fa0a9e447d6b129
Tien Pham <r3dsky02@yahoo.com.vn>
2018-04-15 14:16:26 +0300

    Additional functionalities

--- src/index.js
+++ src/index.js
@@ -3,27 +3,56 @@ import { render } from 'react-dom'
 import { withFormik, Form, Field } from 'formik'
 import Yup from 'yup'
 
-const App = ({
-  values,
-  handleChange
-}) => (
+//touched : visited or not
+const App = ({ values, errors, touched, isSubmitting }) => (
   <Form>
-    <Field type="email" name="email" placeholder="Email" />
-    <Field type="password" name="password" placeholder="Password" />
-    <button>Submit</button>
+    <div>
+      {touched.email && errors.email && <p>{errors.email}</p>}
+      <Field type="email" name="email" placeholder="Email" />
+    </div>
+    <div>
+      {touched.email && errors.password && <p>{errors.password}</p>}
+      <Field type="password" name="password" placeholder="Password" />
+    </div>
+    <label>
+      <Field type="checkbox" name="newsletter" checked={values.newsletter} />
+      Join our newsletter
+    </label>
+    <Field component="select" name="plan">
+      <option value="free">Free</option>
+      <option value="premium">Premium</option>
+    </Field>
+    <button disabled={isSubmitting}>Submit</button>
   </Form>
 )
 
 const FormikApp = withFormik({
-  mapPropsToValues({ email, password }) {
+  mapPropsToValues({ email, password, newsletter, plan }) {
     return {
       email: email || '',
-      password: password || ''
+      password: password || '',
+      newsletter: newsletter || false,
+      plan: plan || 'free'
     }
   },
-  handleSubmit(values) {
-    console.log(values)
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email('Email not valid')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(9, 'Password should > 9 chars')
+      .required('Pass kinda required')
+    //Give us a set of errors
+  }),
+  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+    setTimeout(() => {
+      if (values.email === 'tien@tien.com') {
+        setErrors({ email: 'That email is already taken' })
+      } else {
+        resetForm()
+      }
+    }, 2000)
   }
 })(App)
 
-render(<FormikApp email='tien@tien.com' />, document.getElementById('root'))
+render(<FormikApp email="tien@tien.com" />, document.getElementById('root'))
